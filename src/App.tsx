05c7c5474268324c6911944620e5e23a29259dad
Damir <adidukik@gmail.com>
2023-07-12 12:24:17 +0600

    Merging conflict resolved folders relocated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-import ChatBot from './ChatBot';
-import 'primereact/resources/themes/lara-light-blue/theme.css';
-=======
 import ChatBot from './components/ChatBot';
->>>>>>> a2d0a3cd3c4ae19e506f9222fe20814c46040c0e
+import 'primereact/resources/themes/lara-light-blue/theme.css';
 
 function App() {
   return (
