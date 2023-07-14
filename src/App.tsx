25d510f6ee0fd7a17a9136fbcabe1a027fae0a8d
Damir <adidukik@gmail.com>
2023-07-14 11:37:20 +0600

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css';
 import ChatBot from './components/ChatBot/ChatBot.jsx';
+import BigNumbers from './components/BigNumbers/BigNumbers.tsx';
 import 'primereact/resources/themes/lara-light-blue/theme.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <BigNumbers />
         <ChatBot />
-      </header>
     </div>
   );
 }
---
+++ src/components/BigNumbers/BigNumbers.tsx
@@ -0,0 +1,30 @@
+import { Button } from 'primereact/button';
+import { Chart } from 'primereact/chart';
+import { Column } from 'primereact/column';
+import { DataTable } from 'primereact/datatable';
+import { Menu } from 'primereact/menu';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { ProductService } from '../demo/service/ProductService';
+import { LayoutContext } from '../layout/context/layoutcontext';
+import Link from 'next/link';
+import { Demo } from '../types/types';
+import { ChartData, ChartOptions } from 'chart.js';
+
+import { Card } from 'primereact/card';
+        
+
+interface BigNumbersProps {
+}
+
+const BigNumbers: React.FC<BigNumbersProps> = (props) => {
+  return (
+    <Card title="Title">
+    <p className="m-0">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+        numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+    </p>
+</Card>
+  );
+};
+
+export default BigNumbers;
