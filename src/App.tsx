664e055fb2895b8a349c77f474f699bc52037d92
Damir <adidukik@gmail.com>
2023-07-17 13:21:45 +0600

    Title component created

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,27 @@ import ChatBot from './components/ChatBot/ChatBot.jsx';
 import 'primereact/resources/themes/lara-light-blue/theme.css';
 import NavBar from './components/NavBar/NavBar.tsx';
 import Regions from './components/Regions/Regions.tsx';
+import Title from './components/Title/Title.tsx';
 
 function App() {
   return (
     <div className="App">
-    <div className="Title" style={{background: "red"}}></div>
+    <div className="Title" style={{background: "red"}}>
+      <Title />
+    </div>
     <div className="TabMenu" style={{background: "green"}}></div>
     <div className="BigNumbers" style={{background: "yellow"}}></div>
     <div className="ScrollingText" style={{background: "magenta"}}></div>
     <div className="Regions" style={{background: "blue"}}></div>
-    <div className="Map" style={{background: "#e5e5e5"}}></div>
-    <div className="ChatBot" style={{background: "black"}}></div>
-    <div className="Graph"></div>
+    <div className="Map" style={{background: "#e5e5e5"}}>
+      <Map />
+    </div>
+    <div className="ChatBot" style={{background: "black"}}>
+      {/* <ChatBot /> */}
+    </div>
+    <div className="Graph">
+      {/* <Graph /> */}
+    </div>
 </div>
   );
 }
---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,15 @@
+import { Card } from 'primereact/card';
+import { Skeleton } from 'primereact/skeleton';
+
+const Title = () => {
+
+    return (
+        <Card>
+
+                {/* СИТУАЦИОННО-АНАЛИТИЧЕСКИЙ ЦЕНТР ТОПЛИВНО-ЭНЕРГЕТИЧЕСКОГО КОМПЛЕКСА РЕСПУБЛИКИ КАЗАХСТАН */}
+
+        </Card>
+    );
+};
+
+export default Title;
