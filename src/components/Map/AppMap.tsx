cb1a996f8603a58494299139864498c225eb0e3d
Abay <abay.darmenov@gmail.com>
2023-07-20 17:43:50 +0600

    Added overlay for map when hovering

--- src/components/Map/AppMap.tsx
+++ src/components/Map/AppMap.tsx
@@ -17,6 +17,11 @@ import fieldsData from "../../assets/geo/fields.json";
 import { getCenter } from "ol/extent";
 import { regionNames } from "../Regions/Regions";
 import Stroke from "ol/style/Stroke";
+import { useSelector } from "react-redux";
+import { BigNumberState } from "../BigNumbers/bigNumbersSlice";
+import { toStringHDMS } from "ol/coordinate";
+import Overlay from "ol/Overlay";
+import { Popover } from "react-bootstrap";
 
 const regionsStyle = new Style({
   fill: new Fill({
@@ -33,25 +38,27 @@ const regionsFeatures = format.readFeatures(regionsData);
 const fieldsFeatures = format.readFeatures(fieldsData).filter((feature) => {
   return feature.values_?.type === "добыча";
 });
-
-const getFieldsLayer = () => {
-  const currentType = "нефть";
-  const featuresToDisplay = fieldsFeatures.filter((field) =>
-    String(field.values_?.field_resources).includes(currentType)
-  );
-  return useMemo(
-    () =>
-      new VectorLayer({
-        source: new VectorSource({
-          features: featuresToDisplay,
-        }),
-        style: function (feature) {
-          fieldsStyle.getFill().setColor("#000");
-          return fieldsStyle;
-        },
+const numberToLayerType = {
+  0: "нефть",
+  1: "газ",
+};
+const getFieldsLayer = (bigNumberValue) => {
+  console.log("hi hi", bigNumberValue);
+  const currentType = numberToLayerType["0"];
+  if (currentType) {
+    const featuresToDisplay = fieldsFeatures.filter((field) =>
+      String(field.values_?.field_resources).includes(currentType)
+    );
+    return new VectorLayer({
+      source: new VectorSource({
+        features: featuresToDisplay,
       }),
-    []
-  );
+      style: function (feature) {
+        fieldsStyle.getFill().setColor("#000");
+        return fieldsStyle;
+      },
+    });
+  }
 };
 
 interface AppMapProps {
@@ -59,21 +66,17 @@ interface AppMapProps {
 }
 
 const AppMap = ({ currentRegion }: AppMapProps) => {
+  const bigNumberValue = useSelector((state: BigNumberState) => state.value);
   const regionNameToColor = {};
   const colors = ["#e9cfdb", "#faf0dd", "#d2e9ce", "#d0ebdb"];
   for (let i = 0; i < regionNames.length; i++) {
     regionNameToColor[regionNames[i]] = colors[i % colors.length];
   }
+  const popupRef = useRef(null);
   const [zoom, setZoom] = useState(4);
   const [mapCenter, setMapCenter] = useState([
     7347086.392356056, 6106854.834885074,
   ]);
-
-  const set1 = new Set();
-  const set2 = new Set();
-
-  console.log(set1);
-  console.log(set2);
   const view = useMemo(
     () => new View({ center: mapCenter, zoom: zoom }),
     [mapCenter, zoom]
@@ -132,24 +135,30 @@ const AppMap = ({ currentRegion }: AppMapProps) => {
     [regionsData, regionsStyle]
   );
 
-  const fieldsLayer = getFieldsLayer();
-
   regionsLayer.setOpacity(0.8);
-
+  console.log(bigNumberValue);
   const ref = useRef<HTMLDivElement>(null);
   const mapRef = useRef<Map>(null);
+  let lastIdRef = useRef<number>(-1);
+
+  const [popupText, setPopupText] = useState("");
+  const [popupVisibility, setpopupVisibility] = useState(false);
+
   useEffect(() => {
+    const fieldsLayer = getFieldsLayer(bigNumberValue);
+    const mapLayers = [new TileLayer({ source: new OSM() }), regionsLayer];
+    if (fieldsLayer) mapLayers.push(fieldsLayer);
     //new TileLayer({ source: new OSM() }),
     if (ref.current && !mapRef.current) {
       mapRef.current = new Map({
-        layers: [
-          new TileLayer({ source: new OSM() }),
-          regionsLayer,
-          fieldsLayer,
-        ],
+        layers: mapLayers,
         view: view,
         target: ref.current,
       });
+      const popup = new Overlay({
+        element: document.getElementById("popup"),
+      });
+      mapRef.current.addOverlay(popup);
 
       // Showing place features when the user hovers
 
@@ -168,18 +177,30 @@ const AppMap = ({ currentRegion }: AppMapProps) => {
         if (selected !== null) {
           selected.setStyle(undefined);
           selected = null;
+          console.log(e);
         }
-
+        setpopupVisibility(false);
         mapRef.current.forEachFeatureAtPixel(e.pixel, function (f) {
-          console.log(f);
+          if (f.values_.type !== "district") setpopupVisibility(true);
+          if (
+            f.values_.type !== "district" &&
+            lastIdRef.current !== f.values_.id
+          ) {
+            console.log(f.values_.operator_name);
+            console.log(e.coordinate);
+            popup.setPosition(e.coordinate);
+            setPopupText(f.values_.operator_name);
+            lastIdRef.current = f.values_.id;
+          }
           selected = f;
           selectStyle.getFill().setColor(f.get("COLOR") || "#eeeeee");
           f.setStyle(selectStyle);
+
           return true;
         });
       });
     }
-  }, [ref, mapRef, mapCenter, zoom, regionsLayer]);
+  }, [ref, mapRef, mapCenter, zoom, regionsLayer, bigNumberValue]);
 
   if (currentRegion) {
     console.log(currentRegion);
@@ -194,10 +215,16 @@ const AppMap = ({ currentRegion }: AppMapProps) => {
       console.log(e);
     });
   }
-
   return (
     <>
       <div ref={ref} id="map" />
+      <div
+        id="popup"
+        ref={popupRef}
+        className={popupVisibility ? "" : "hidden"}
+      >
+        {popupText}
+      </div>
     </>
   );
 };
