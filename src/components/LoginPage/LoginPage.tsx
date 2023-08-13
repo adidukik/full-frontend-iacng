e743441b862de0cf9af987726737d5ee34e6bd72
Damir Zhumatayev <adidukik@gmail.com>
2023-08-12 21:19:44 -0700

    Login page designed, background changed

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState } from 'react';
+import { TextField, Button, Container, Typography, CssBaseline, Card, CardContent, Box } from '@mui/material';
+import './LoginPage.css';
 
-const LoginPage = () => {
-  return <div>LoginPage</div>;
+const LoginPage: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Implement your login logic here
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  return (
+    <Box
+      sx={{
+        fontFamily: 'Montserrat, sans-serif',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        background: 'url("./src/assets/images/bg2.png") center center no-repeat',
+        backgroundSize: 'cover',
+      }}
+    >
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Card className="card">
+          <CardContent>
+            <Typography component="h1" variant="h5" sx={{ color: 'white', marginBottom: 2 }}>
+              Вход в портал ИАЦНГ
+            </Typography>
+            <form onSubmit={(e) => e.preventDefault()}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                label="Логин"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                InputLabelProps={{ style: { color: 'white' } }}
+                InputProps={{ style: { color: 'white', borderColor: 'white' } }}
+                focused
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                label="Пароль"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                InputLabelProps={{ style: { color: 'white' } }}
+                InputProps={{ style: { color: 'white', borderColor: 'white' } }}
+                focused
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                sx={{ marginTop: 2 }}
+                onClick={handleLogin}
+              >
+                Войти
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+      </Container>
+    </Box>
+  );
 };
 
 export default LoginPage;
