d2c5ee72118caf004412b3a1223ae1b968da9e6a
Damir Zhumatayev <adidukik@gmail.com>
2023-08-13 22:11:56 -0700

    ReCaptcha added

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import {
   TextField,
   Button,
@@ -14,6 +14,7 @@ import {
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase';
 import { useNavigate } from 'react-router-dom';
+import ReCAPTCHA from 'react-google-recaptcha'; // Import the reCAPTCHA component
 
 const LoginPage: React.FC = () => {
   const navigate = useNavigate();
@@ -22,11 +23,20 @@ const LoginPage: React.FC = () => {
   const [error, setError] = useState('');
   const [alertVisible, setAlertVisible] = useState(false); // Initialize to false
 
+  const recaptchaRef = useRef<ReCAPTCHA | null>(null); // Create a ref for the reCAPTCHA component
+
   const handleLogin = async () => {
     try {
       setError(''); // Clear any previous error messages
-      await signInWithEmailAndPassword(auth, email, password);
-      navigate('/');
+
+      // Verify the reCAPTCHA response
+      const recaptchaToken = await recaptchaRef.current?.executeAsync();
+
+      if (recaptchaToken) {
+        // reCAPTCHA verified, proceed with authentication
+        await signInWithEmailAndPassword(auth, email, password);
+        navigate('/');
+      }
     } catch (error) {
       setError('Неверный адрес электронной почты или пароль'); // Set the error message
       setAlertVisible(true); // Make sure the alert is visible
@@ -84,6 +94,11 @@ const LoginPage: React.FC = () => {
                 InputLabelProps={{ style: { color: 'white' } }}
                 InputProps={{ style: { color: 'white', borderColor: 'white' } }}
                 focused
+              />
+              <ReCAPTCHA
+                ref={recaptchaRef}
+                sitekey="6Ld_9KMnAAAAAB_hOrFyDlbpCyPGZRV_X_zze-BF"
+                size="invisible"
               />
               <Button
                 type="submit"
