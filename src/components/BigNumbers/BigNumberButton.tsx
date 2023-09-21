e3900d61d72d12595a966b9c073f13c5290d3437
Damir Zhumatayev <adidukik@gmail.com>
2023-09-21 14:42:18 +0900

    bignumber button outline-dark

--- src/components/BigNumbers/BigNumberButton.tsx
+++ src/components/BigNumbers/BigNumberButton.tsx
@@ -22,7 +22,7 @@ export const BigNumberButton = ({ bigNumber, onClick }) => {
 
   return (
     <Button
-      variant="outline-primary"
+      variant="outline-dark"
       className={`big-numbers__btn ${
         currentBigNumberId === id && "big-numbers__btn-active"
       }`}
