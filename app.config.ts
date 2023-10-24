49e27b6c0d151919615c58a7ab638c508987632c
Abay <abay.darmenov@gmail.com>
2023-10-24 19:38:18 +0600

    change ip in config

--- app.config.ts
+++ app.config.ts
@@ -2,5 +2,5 @@ export const APP_CONFIG = {
   DEFAULT_BACKEND_URL: "",
   // for opec mainly
   SECONDARY_BACKEND_URL: "http://192.168.0.57:8000",
-  ADDITIONAL_BACKEND_URL: "http://192.168.0.115:8001",
+  ADDITIONAL_BACKEND_URL: "http://192.168.0.101:8001",
 };
